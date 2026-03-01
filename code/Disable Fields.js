930e1e338cfeb9004efd774d19c5bcdbd0c1b3c6
Dharmateja15 <60874800+Dharmateja15@users.noreply.github.com>
2026-03-01 16:35:02 +0530

    Delete code/Disable Fields.js
    Remove wrongly placed Disable Fields.js file

--- code/Disable Fields.js
+++
@@ -1,8 +0,0 @@
-function onLoad() {
-
-   // Disable calculated fields
-   g_form.setDisabled('u_total', true);
-   g_form.setDisabled('u_percentage', true);
-   g_form.setDisabled('u_result', true);
-
-}
