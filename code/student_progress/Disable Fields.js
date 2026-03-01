d45602f3a9ac68edd500c75e6ea3da9ef63f0893
Dharmateja15 <60874800+Dharmateja15@users.noreply.github.com>
2026-03-01 16:30:56 +0530

    Add function to disable calculated fields on load

---
+++ code/student_progress/Disable Fields.js
@@ -0,0 +1,8 @@
+function onLoad() {
+
+   // Disable calculated fields
+   g_form.setDisabled('u_total', true);
+   g_form.setDisabled('u_percentage', true);
+   g_form.setDisabled('u_result', true);
+
+}
