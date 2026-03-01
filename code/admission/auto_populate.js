50c00f97f53b126b29764ff8bd8889afb46625a4
Dharmateja15 <60874800+Dharmateja15@users.noreply.github.com>
2026-03-01 16:22:18 +0530

    Add function to auto-populate admission fields
    Implement auto-population of admission fields based on admission number.

---
+++ code/admission/auto_populate.js
@@ -0,0 +1,27 @@
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+
+   if (isLoading || !newValue) {
+      return;
+   }
+
+   g_form.getReference('u_admission_number', function(a) {
+
+      if (!a) return;
+
+      g_form.setValue('u_admin_date', a.u_admin_date);
+      g_form.setValue('u_grade', a.u_grade);
+      g_form.setValue('u_student_name', a.u_student_name);
+      g_form.setValue('u_father_name', a.u_father_name);
+      g_form.setValue('u_mother_name', a.u_mother_name);
+      g_form.setValue('u_father_cell', a.u_father_cell);
+      g_form.setValue('u_mother_cell', a.u_mother_cell);
+
+      g_form.setDisabled('u_admin_date', true);
+      g_form.setDisabled('u_grade', true);
+      g_form.setDisabled('u_student_name', true);
+      g_form.setDisabled('u_father_name', true);
+      g_form.setDisabled('u_mother_name', true);
+      g_form.setDisabled('u_father_cell', true);
+      g_form.setDisabled('u_mother_cell', true);
+   });
+}
